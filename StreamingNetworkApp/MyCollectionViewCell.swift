5c1a856162bde382c74353b1e951406db53e958d
Jeroen de Bie <Jeroendebie@live.nl>
2017-03-09 12:06:43 +0100

    First commit Setup of project

---
+++ StreamingNetworkApp/MyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollectionViewCell.swift
+//  StreamingNetworkApp
+//
+//  Created by Jeroen de Bie on 08/03/2017.
+//  Copyright © 2017 Jeroen de Bie. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
---
+++ StreamingNetworkApp/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  StreamingNetworkApp
+//
+//  Created by Jeroen de Bie on 08/03/2017.
+//  Copyright © 2017 Jeroen de Bie. All rights reserved.
+//
+
+import UIKit
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    var items = ["247Jazz", "247Blitz", "247Spice", "247Ultra", "247Event", "Q-Dance", "247The Beat", "Downstation Buddha"]
+    
+    // MARK: - UICollectionViewDataSource protocol
+    
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.myLabel.text = self.items[indexPath.item]
+        cell.backgroundColor = UIColor.black
+        cell.layer.borderColor = UIColor.darkGray.cgColor
+        cell.layer.borderWidth = 3
+        cell.layer.cornerRadius = 50
+        // make cell more visible in our example project
+        
+        // change background color when user touches cell
+        func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+            let cell = collectionView.cellForItem(at: indexPath)
+            cell?.backgroundColor = UIColor.red
+        }
+        
+        // change background color back when user releases touch
+        func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+            let cell = collectionView.cellForItem(at: indexPath)
+            cell?.backgroundColor = UIColor.cyan
+        }
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
+
+
+
